275a55f35c04ce0b0044623aec97c2c1ec5ee8d8
Abhiro0p <09abhiroop@gmail.com>
2025-04-01 08:30:04 +0530

    tsconfig.ts

---
+++ tsconfig.ts
@@ -0,0 +1,15 @@
+{
+  "compilerOptions": {
+    "target": "es5",
+    "module": "esnext",
+    "moduleResolution": "node",
+    "strict": true,
+    "baseUrl": "./",
+    "outDir": "./app/dependencies",
+    "paths": {
+      "@app/*": ["app/*"]
+    }
+  },
+  "include": ["app/**/*.ts"],
+  "exclude": ["node_modules"]
+}
